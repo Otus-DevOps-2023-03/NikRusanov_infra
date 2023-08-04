639c51a4600cff722c660689f579d959142aa51b
NikRusanov <kenya.rusanov@gmail.com>
2023-08-05 02:31:27 +0300

    Delete yandex provider init from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-
-    }
-
-  }
-
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#
+#    }
+#
+#  }
+#
+#}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
